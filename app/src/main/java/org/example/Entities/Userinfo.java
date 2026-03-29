5294b3dd5ebbc9564fa5a8fc1d8675666b47bf41
ashlurk <ashlurk367@gmail.com>
2026-03-29 23:41:04 +0530

    First

---
+++ app/src/main/java/org/example/Entities/Userinfo.java
@@ -0,0 +1,47 @@
+package org.example.Entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+
+public class Userinfo {
+
+    @Id
+    @Column(name = "user_id")
+    private long user_id;
+
+    private String username;
+
+
+    private String password;
+
+    @ManyToAny(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<UserRole> roles = new HashSet<>();
+
+
+    
+}
